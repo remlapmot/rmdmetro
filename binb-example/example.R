6ce81ca4e71d0939b67879a39dd1edfa3fb7e756
Tom Palmer <remlapmot@hotmail.com>
2020-12-23 13:28:17 +0000

    Create example.R

---
+++ binb-example/example.R
@@ -0,0 +1,4 @@
+rmarkdown::draft("myslides.Rmd", template = "metropolis",
+                package = "binb", edit = FALSE)
+setwd("myslides")       ## template creates a new subdir
+rmarkdown::render("myslides.Rmd")
